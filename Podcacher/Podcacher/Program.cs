eb62568967a12f549e7cefefc297cd00a50ebc8e
Tiernan OToole <tiernan@tiernanotoole.ie>
2015-02-17 09:41:00 +0000

    basic owin host

--- Podcacher/Podcacher/Program.cs
+++ Podcacher/Podcacher/Program.cs
@@ -10,6 +10,11 @@ namespace Podcacher
     {
         static void Main(string[] args)
         {
+            using (Microsoft.Owin.Hosting.WebApp.Start<Startup>("http://localhost:9000"))
+            {
+                Console.WriteLine("Press [enter] to quit...");
+                Console.ReadLine();
+            }
         }
     }
 }
---
+++ Podcacher/Podcacher/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Podcacher.Startup))]
+
+namespace Podcacher
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseWelcomePage();
+        }
+    }
+}
